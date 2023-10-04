7e6aa66be4e2ca81e26c108ce6164c46039ada4c
Naga Nitish <95007559+NagaNitishChowdary@users.noreply.github.com>
2023-10-04 23:08:28 +0530

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,24 @@
+import java.util.* ;
+import java.io.* ; 
+
+public class Main {
+    public static void main(String[] args){
+        
+        // STACK SYNTAX
+        // Stack<type> st = new Stack<type>() ;
+        // st.push() , st.peek() , st.pop() 
+        
+        Stack<Integer> st = new Stack<Integer>() ; 
+
+        for(int i=0 ; i<5 ; i++) st.push(i) ;
+        // while(!st.empty()){
+        //     System.out.print(st.peek() + " ") ;
+        //     st.pop() ;
+        // }
+
+        while(!st.empty()){
+            System.out.print(st.pop() + " ") ;
+        }
+
+    }
+}
